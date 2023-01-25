0dcc690ffc7dd85727499dc264d2e033a5c7060c
MartinArielMendoza <marttx2022@gmail.com>
2023-01-25 02:16:24 -0300

    implementando api

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
-import { useEffect} from 'react';
+import Pokemones from './components/pokemones';
 
 function App() {
-  useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
-    .then((response) =>
-    response.json())
-    .then((data) =>{
-    console.log(data)
-    })
-  },[])
-
-
-
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting= 'Bienvenidos'/>
+      <Pokemones/>
       <ItemCount onAdd={(count) => console.log(count)} stock={100}/>
     </div>
   );
-}
+
+
+  }
+
 
 export default App;
